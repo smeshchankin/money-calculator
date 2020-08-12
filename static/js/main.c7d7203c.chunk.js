1f60a7eb21a4f0250499d6d794be209547da4cd6
Sergey Meshchankin <smeshchankin@gmail.com>
2020-08-12 23:38:48 +0300

    Updates

---
+++ static/js/main.c7d7203c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmoney-calculator"]=this["webpackJsonpmoney-calculator"]||[]).push([[0],{3:function(e,t,n){e.exports=n(8)},8:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),c=n(2);var l=function(){return r.a.createElement("div",null,"Money Calculator")};n.n(c).a.render(r.a.createElement(r.a.StrictMode,null,r.a.createElement(l,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.c7d7203c.chunk.js.map
