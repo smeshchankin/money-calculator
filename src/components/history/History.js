f52b70fd005e21966bee191a018d7341619b4d50
Sergey Meshchankin <smeshchankin@gmail.com>
2020-08-14 00:42:22 +0300

    [MC-002] React component: HistoryItem

--- src/components/history/History.js
+++ src/components/history/History.js
@@ -1,19 +1,12 @@
 import React from 'react';
+import HistoryItem from './HistoryItem';
 
 export default function() {
     return (
         <section className="history">
             <h3>History</h3>
             <ul className="history__list">
-                <li className="history__item history__item-plus">Salary
-                    <span className="history__money">$ +3000</span>
-                    <button className="history__delete">x</button>
-                </li>
-
-                <li className="history__item  history__item-minus">Food
-                    <span className="history__money">$ -100</span>
-                    <button className="history__delete">x</button>
-                </li>
+                <HistoryItem />
             </ul>
         </section>
     );
---
+++ src/components/history/HistoryItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HistoryItem = () => (
+    <li className="history__item history__item-plus">Salary
+        <span className="history__money">$ +3000</span>
+        <button className="history__delete">x</button>
+    </li>
+);
+
+export default HistoryItem;
+
+/*
+<li className="history__item  history__item-minus">Food
+    <span className="history__money">$ -100</span>
+    <button className="history__delete">x</button>
+</li>
+*/
