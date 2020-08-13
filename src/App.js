885cbb1e25d5e90c2c302294134f0baf7020351b
Sergey Meshchankin <smeshchankin@gmail.com>
2020-08-14 00:52:34 +0300

    [MC-002] Class component App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Total from './components/total/Total';
 import History from './components/history/History';
 import Operation from './components/operation/Operation';
 
-function App() {
-  return (
-    <>
-      <header>
-        <h1>Wallet</h1>
-        <h2>Money Calculator</h2>
-      </header>
+class App extends Component {
 
-      <main>
-        <div className="container">
-            <Total />
-            <History />
-            <Operation />
-        </div>
-      </main>
-    </>
-  );
+  render() {
+    return (
+      <>
+        <header>
+          <h1>Wallet</h1>
+          <h2>Money Calculator</h2>
+        </header>
+
+        <main>
+          <div className="container">
+              <Total />
+              <History />
+              <Operation />
+          </div>
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
